0d4cd4ce8e3e6a7aaddec757bd4249600a4f32a6
pramay_usadadiya <info.prem07@gmail.com>
2025-05-27 14:59:40 +0530

    Add Main component: Implement authentication check and redirect to login if not logged in

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
-import Main from './Main';
+import Main from './Mains';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
